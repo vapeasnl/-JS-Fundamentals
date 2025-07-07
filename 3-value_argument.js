18d9888f6b43c4482c50e6336aed8e5ba6abf646
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-07-07 08:00:45 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,1 +1,11 @@
+const {argv} = require('node:process')
 
+if (argv[2]){
+    argv.forEach((arg, index) => {
+        if (index == 2){
+            console.log(arg)
+        }
+    })
+}else{
+    console.log("No argument")
+}
