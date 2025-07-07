9e440724473cf89fbbd309aea8f5acec0fbe88ab
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-07-07 08:01:20 +0100

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,1 +1,10 @@
+const { argv } = require('node:process')
 
+if (argv[2]){
+    let num = Number(argv[2])
+    if (num){
+        console.log(`My number: ${num}`)
+    }else{
+        console.log('Not a number')
+    }
+}
