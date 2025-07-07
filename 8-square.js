f9c74973245904f811f2872d602ee9d1c212340c
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-07-07 08:02:07 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,1 +1,18 @@
+const {argv} = require('node:process')
 
+if (argv[2]){
+    let num = Number(argv[2])
+    if (num != NaN && num > 0){
+        for (let i = 0; i < num; i++){
+            let result = ""
+            for (let j = 0; j < num; j++){
+                result += "X"
+            }
+            console.log(result)
+        }
+    }else{
+    console.log("Missing size")
+}
+}else{
+    console.log("Missing size")
+}
