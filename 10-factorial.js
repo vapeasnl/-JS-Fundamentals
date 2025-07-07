deed13fc90303a3a9c07f176e1f6e53ef6ae200a
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-07-07 08:00:16 +0100

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,1 +1,18 @@
+const {argv} = require('node:process')
 
+if(argv[2]){
+    let num = Number(argv[2])
+
+    function factorial(n){
+        if (n == 0 || n == 1){
+            return 1
+        }else{
+        return (n * factorial(n - 1))
+        }
+        
+    }
+
+    console.log(factorial(num))
+}else{
+    console.log(1)
+}
