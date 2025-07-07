2c045a6537b3366ea5bc4b68bd613a1671ec677f
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-07-07 08:01:52 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,1 +1,12 @@
+const {argv} = require('node:process')
 
+if (argv[2]){
+    let num = Number(argv[2])
+    if (num != NaN && num > 0){
+        for (let i = 0; i < num; i++){
+            console.log("C is fun")
+        }
+    }
+}else{
+    console.log("Missing number of occurrences")
+}
