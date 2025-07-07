a1a6b07d6510b40ba3daa99da668e3727a27103a
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2025-07-07 08:02:25 +0100

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,1 +1,14 @@
+const {argv} = require('node:process')
 
+if (argv[2] && argv[3]){
+    let num1 = Number(argv[2])
+    let num2 = Number(argv[3])
+    if (num1 != NaN  && num2 != NaN ){
+        let result = num1 + num2
+        console.log(result)
+    }else{
+        console.log(NaN)
+    }
+}else{
+        console.log(NaN)
+    }
